c808be0826eb7bee4166438c62eadea60bf923d4
VthanhHai <101922208+VthanhHai@users.noreply.github.com>
2022-06-25 20:11:53 +0700

    Add files via upload

---
+++ hang.cs
@@ -0,0 +1,23 @@
+﻿using  System.Text;
+using System.Threading.Tasks;
+namespace Hang
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            /*
+            - Hằng là một biến những giá trị không thay đổi trong suốt chương trình.
+            - Bắt buộc phải khởi tạo giá trị khi khai báo
+            - để ngăn chặn việc gán giá trị khác vào.
+            - Hằng làm cho chương trình dễ nâng cấp, dễ sửa hơn.
+            - chia làm ba loại hằng giá trị, biểu tượng và kiểu liệt kê.
+            */
+            const int a = 10;
+            
+            const int x = a; // thêm const, x là giá trị hằng 
+            Console.WriteLine(x);
+            Console.ReadKey();
+        }
+    }
+}
